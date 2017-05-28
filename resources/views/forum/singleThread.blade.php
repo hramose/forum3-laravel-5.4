7917498b62631553501e998bc29dfab37a564854
simonjsuh <simon.j.suh@gmail.com>
2017-05-28 13:08:17 -0700

    tutorial part 2
    code

--- resources/views/forum/singleThread.blade.php
+++ resources/views/forum/singleThread.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="padding-bottom: 100px;">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
           <div class="panel panel-primary">
@@ -21,6 +21,18 @@
               </div>
             </div>
           @endforeach
+
+          @if (!Auth::check())
+            <div>To post a reply, please <a href="{{ route('login') }}">login</a>.</div>
+          @else
+            <form action="">
+              <div class="form-group">
+                <textarea name="reply" id="" cols="30" rows="7" class="form-control" placeholder="Enter your reply here"></textarea>
+              </div>
+
+              <input type="submit" class="btn btn-primary" value="Submit Reply">
+            </form>
+          @endif
         </div>
     </div>
 </div>
